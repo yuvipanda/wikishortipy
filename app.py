a04eaa1e88775a8a8eac4084f5c623a67f875d5e
YuviPanda <yuvipanda@gmail.com>
2011-12-30 15:22:50 +0530

    Added interwiki redirects

--- app.py
+++ app.py
@@ -30,10 +30,32 @@ def url_for_page(lang, id):
             cache.set(url, title)
     return '%s/wiki/%s' % (host, title)
 
+def interwiki_for_page(lang, entitle):
+    host = host_for_lang(lang)
+    url = 'http://en.wikipedia.org/w/api.php?action=query&prop=langlinks&lllimit=500&format=json&titles=%s' % (entitle, )
+    data = cache.get(url)
+    if data == None:
+        res = requests.get(url)
+        data = json.loads(res.content)
+    title = '' 
+    if '-1' not in data['query']['pages']:
+        langlinks = data['query']['pages'].values()[0]['langlinks']
+        for langlink in langlinks:
+            if langlink['lang'] == lang:
+                title = langlink['*']
+                break
+
+    return '%s/wiki/%s' % (host, title)
+
+
 @app.route('/s/<lang>/<shortkey>/')
 def do_redirect(lang, shortkey):
     page_id = int(shortkey, 36)
     return redirect(url_for_page(lang, page_id))
+
+@app.route('/en/<lang>/<entitle>/')
+def interwiki_redirect(lang, entitle):
+    return redirect(interwiki_for_page(lang, entitle))
 
 if __name__ == '__main__':
     app.run()
